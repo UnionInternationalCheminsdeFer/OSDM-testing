e27f37de2b3fc77d282f1827f02976ed4b32bec2
Josef Petrak <josef@petrak.xyz>
2023-08-04 11:11:04 +0200

    WiP

--- library/functions.js
+++ library/functions.js
@@ -2,5 +2,5 @@ setAuthToken = function () {
     let jsonData = JSON.parse(responseBody);
     
     pm.globals.set(GV.ACCESS_TOKEN, jsonData.access_token);
-    pm.globals.set("access_token", jsonData.access_token);
+    //pm.globals.set("access_token", jsonData.access_token);
 }
