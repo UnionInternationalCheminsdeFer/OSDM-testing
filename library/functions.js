9f54290576e5b893474843117626bbf6b720b630
Josef Petrak <josef@petrak.xyz>
2023-08-15 12:23:16 +0200

    WiP

--- library/functions.js
+++ library/functions.js
@@ -2,5 +2,19 @@ setAuthToken = function () {
     let jsonData = JSON.parse(responseBody);
     
     pm.globals.set(GV.ACCESS_TOKEN, jsonData.access_token);
-    //pm.globals.set("access_token", jsonData.access_token);
 }
+
+osdmOfferSearchCriteria = function (
+    currency,
+    offerParts,
+    serviceClassTypes,
+    comfortClasses,
+) {
+    var offerSearchCriteria = new Object();
+
+    if (!currency) {
+        offerSearchCriteria.currency = currency;
+    }
+
+    pm.globals.set('offer_search_criteria', offerSearchCriteria);
+};
