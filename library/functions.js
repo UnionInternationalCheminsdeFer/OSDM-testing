198249e0d96dc81044bac3ba07bb9a1f9fb780a5
Tim van den Berg <tim.vandenberg@sqills.com>
2023-12-15 15:12:59 +0100

    Updated for both environments, added validations for booking

--- library/functions.js
+++ library/functions.js
@@ -127,18 +127,18 @@ validateOfferConformsToOfferSearchCriteria = function (offer) {
 };
 
 
-validateOfferResponse = function(tripSpecifications, passengerSpecifications, searchCriteria, fulfillmentOptions, offers, scenarioType) {
+validateOfferResponse = function(tripSpecifications, passengerSpecifications, searchCriteria, fulfillmentOptions, offers, trips, scenarioType) {
 	
 	pm.test("offers are returned", function () {
 	    pm.expect(offers).not.to.be.empty;
 
-        console.log("trip:"+pm.globals.get(OFFER.TRIP_SPECIFICATIONS));
-        console.log(anonymousPassengerSpecifications);
-        console.log(pm.globals.get(OFFER.FULFILLMENT_OPTIONS));
-        console.log(searchCriteria);
-        console.log(offers);
-        console.log(anonymousPassengerSpecifications);
-        console.log("type:"+pm.globals.get("SCENARIO_TYPE"));
+        validationLogger("trip:"+pm.globals.get(OFFER.TRIP_SPECIFICATIONS));
+        validationLogger(passengerSpecifications);
+        validationLogger(pm.globals.get(OFFER.FULFILLMENT_OPTIONS));
+        validationLogger(searchCriteria);
+        validationLogger(offers);
+        //validationLogger(anonymousPassengerSpecifications);
+        validationLogger("type:"+pm.globals.get("SCENARIO_TYPE"));
 
         
 	    var requireAdmission = false;
@@ -148,6 +148,9 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
 	    switch(scenarioType) {
 	      case "BOTH":
 	        requireAdmission = true;
+	        requireReservation = true;
+	        break;
+	      case "RESERVATION":
 	        requireReservation = true;
 	        break;
 	      default:
@@ -164,10 +167,10 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
 	        var items_found = 0;
 	        var passengerRef = "";
 	        
-	        console.log("There are "+offerLength+" offers available");
+	        validationLogger("There are "+offerLength+" offers available");
 	
 	        while(found==false && offerIndex < offerLength) {
-	            console.log("checking offer: "+offerIndex);
+	            validationLogger("checking offer: "+offerIndex);
 	            offer = offers[offerIndex];
 
                 var foundAdmissions = 0;
@@ -185,12 +188,12 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
 		                var admissionOfferPart = offer.admissionOfferParts[admissionOfferPartsIndex];
 
                          var passengerIndex = 0;
-                         var passengerLenght = anonymousPassengerSpecifications.length;
+                         var passengerLenght = passengerSpecifications.length;
 
                          admissionOfferPart.passengerRefs.forEach(function(passengerRef){
                             while(passengerIndex<passengerLenght){
                                 
-                                if(anonymousPassengerSpecifications[passengerIndex].externalRef==passengerRef){
+                                if(passengerSpecifications[passengerIndex].externalRef==passengerRef){
 
                                     //check if fullfillment options were requested
                                     if(fulfillmentOptions!=undefined) {
@@ -234,11 +237,11 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
 		                var ancillaryOfferPart = offer.ancillaryOfferParts[ancillaryOfferPartsIndex];
 	
                         var passengerIndex = 0;
-                        var passengerLenght = anonymousPassengerSpecifications.length;
+                        var passengerLenght = passengerSpecifications.length;
                         ancillaryOfferPart.passengerRefs.forEach(function(passengerRef){
                             while(passengerIndex<passengerLenght){
                                 
-                                if(anonymousPassengerSpecifications[passengerIndex].externalRef==passengerRef){
+                                if(passengerSpecifications[passengerIndex].externalRef==passengerRef){
                                     foundAncillaries++;
                                 }
 
@@ -260,11 +263,11 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
 		                var reservationOfferPart = offer.reservationOfferParts[reservationOfferPartsIndex];
 
                         var passengerIndex = 0;
-                        var passengerLenght = anonymousPassengerSpecifications.length;
+                        var passengerLenght = passengerSpecifications.length;
                         reservationOfferPart.passengerRefs.forEach(function(passengerRef){
                             while(passengerIndex<passengerLenght){
                                 
-                                if(anonymousPassengerSpecifications[passengerIndex].externalRef==passengerRef){
+                                if(passengerSpecifications[passengerIndex].externalRef==passengerRef){
 
                                     if(searchCriteria!=undefined&&searchCriteria.currency!=undefined) {
                                         
@@ -309,12 +312,12 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
 		            }
 	            }
 
-                console.log("admissions:"+foundAdmissions);
-	            console.log("ancillaries:"+foundAncillaries);
-                console.log("reservations:"+foundReservations);
+                validationLogger("admissions:"+foundAdmissions);
+	            validationLogger("ancillaries:"+foundAncillaries);
+                validationLogger("reservations:"+foundReservations);
                 
 
-                var amounts = anonymousPassengerSpecifications.length;
+                var amounts = passengerSpecifications.length;
                 switch(scenarioType) {
                 case "BOTH":
                     
@@ -327,6 +330,17 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
                     });
 
                     if(amounts==foundAdmissions&&amounts==foundReservations) {
+                        found = true;
+                    }
+
+                    break;
+                case "RESERVATION":
+
+                    pm.test("Correct reservations are returned", function () {
+                        pm.expect(foundReservations).to.equal(amounts);
+                    });
+
+                    if(amounts==foundReservations) {
                         found = true;
                     }
 
@@ -344,7 +358,7 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
 	            offerIndex++;
 	        }
 	    } else {
-	        console.log("No offers available");
+	        validationLogger("No offers available");
 	    }
 	});
 
@@ -374,7 +388,8 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
                     if(requiredLeg.timedLeg.start.stopPlaceRef.stopPlaceRef==leg.timedLeg.start.stopPlaceRef.stopPlaceRef&&
                         requiredLeg.timedLeg.end.stopPlaceRef.stopPlaceRef==leg.timedLeg.end.stopPlaceRef.stopPlaceRef &&
                         requiredLeg.timedLeg.service.vehicleNumbers[0]==leg.timedLeg.service.vehicleNumbers[0] &&
-                        requiredLeg.timedLeg.service.carriers[0].ref==leg.timedLeg.service.carriers[0].ref) {
+                        requiredLeg.timedLeg.service.carriers[0].ref==leg.timedLeg.service.carriers[0].ref
+                        ) {
                         legsFound++;
                         legFound = true;
                     }
@@ -403,6 +418,320 @@ validateOfferResponse = function(tripSpecifications, passengerSpecifications, se
 	});
 };
 
+validateBookingResponse = function(tripSpecifications, passengerSpecifications, searchCriteria, fulfillmentOptions, offers, offerId, booking, scenarioType) {
 
+	var bookingId = booking.id;
+	var createdOn = new Date(booking.createdOn);
+	var passengers = booking.passengers;
+	var confirmationTimeLimit = new Date(booking.confirmationTimeLimit);
+	var bookedOffers = booking.bookedOffers;
+	
+	validationLogger("Checking booking with Id: "+bookingId);
+	validationLogger("CreatedOn: "+createdOn);
+	validationLogger("Passengers: "+passengers);
+	validationLogger("ConfirmationTimeLimit: "+confirmationTimeLimit);
+	validationLogger("BookedOffers: "+bookedOffers);
+	validationLogger("offers: "+offers);
+	validationLogger("offer_id: "+offerId);
+	
+	//check the bookingId
+	pm.test("a bookingId is returned", function () {
+        pm.expect(bookingId).to.be.a('string').and.not.be.empty;
+    });
+	
+	//check the creation date
+	var currentDate = new Date();
+	validationLogger(currentDate);
+	pm.test("Correct createdOn is returned", function () {
+        pm.expect(currentDate.getDate()).to.equal(createdOn.getDate());
+        pm.expect(currentDate.getMonth()).to.equal(createdOn.getMonth());
+        pm.expect(currentDate.getFullYear()).to.equal(createdOn.getFullYear());
+    });
+    
+    //check if a confirmationTimeLimit is available to check
+    if(booking.confirmationTimeLimit!=undefined&&booking.confirmationTimeLimit!=null){
+	    //check the confirmationTimeLimit
+	    pm.test("a correct confirmationTimeLimit is returned", function () {
+	    	var current = currentDate.getTime();
+	    	var confirmation = confirmationTimeLimit.getTime();
+	        pm.expect(confirmation).to.be.above(current);
+	    });
+    }
+    
+    var offer = null;
+    offers.some(function(internalOffer){
+    	if(internalOffer.offerId==offerId){
+    		offer = internalOffer;
+    		return true;
+    	}
+    });
+    
+    if(offer==undefined||offer==null) {
+    	validationLogger("No correct offer can be found, skipping rest of validation");
+    	return;
+    } else {
+    	validationLogger("Correct offer from offer response found, performing rest of validation");
+    }
+    
+    var found = bookedOffers.some(function(bookedOffer){
+		return compareOffers(bookedOffer, offer, booking);
+	});
+	
+	pm.test("Correct offer "+offer.offerId+" is returned", function () {
+        pm.expect(found).to.equal(true);
+    });
+    
+    //check that all the passengers match the passengers from the offer
+	offer.passengerRefs.forEach(function(passenger){
 
+		var found = false;
+		found = passengers.some(function(bookedPassenger){
+			validationLogger("checking "+bookedPassenger.externalRef+" against "+passenger);
+			if(bookedPassenger.externalRef==passenger){
+				return true;
+			}
+		});
+
+		pm.test("passenger "+passenger+" returned", function () {
+			pm.expect(found).to.equal(true);
+		});
+
+	});
+
+};
+
+compareOffers = function(bookedOffer, offer, booking){
+	validationLogger("Comparing "+bookedOffer.offerId+" to "+offer.offerId);
+	
+	if((bookedOffer.admissions==undefined||bookedOffer.admissions==null||bookedOffer.admissions.length==0)&&
+		(offer.admissionOfferParts==undefined||offer.admissionOfferParts==null||offer.admissionOfferParts.length==0)) {
+		//ok, nothing defined
+		validationLogger("skipping admissions");
+	} else {
+		validationLogger("checking admissions");
+		bookedOffer.admissions.forEach(function(bookedAdmission){
+		
+			//check generic part
+			checkGenericBookedOfferPart(bookedAdmission);
+			
+			var found = offer.admissionOfferParts.some(function(offeredAdmission){
+				return compareAdmissions (bookedAdmission, offeredAdmission, booking);
+			});
+		});	
+	}
+	
+	if((bookedOffer.reservations==undefined||bookedOffer.reservations==null||bookedOffer.reservations.length==0)&&
+		(offer.reservationOfferParts==undefined||offer.reservationOfferParts==null||offer.reservationOfferParts.length==0)) {
+		//ok, nothing defined
+		validationLogger("skipping reservations");
+	} else {
+		validationLogger("checking reservations");
+		bookedOffer.reservations.forEach(function(bookedReservation){
+		
+			//check generic part
+			checkGenericBookedOfferPart(bookedReservation);
+		
+			var found = offer.reservationOfferParts.some(function(offeredReservation){
+				return compareReservations (bookedReservation, offeredReservation, booking);
+			});
+		});	
+	}
+	
+	return true;
+};
+
+compareAdmissions = function(bookedAdmission, offeredAdmission, booking){
+	validationLogger("Comparing admission "+bookedAdmission.id+" to "+offeredAdmission.id);
+	
+	//price "should" be the same
+	pm.test("Price of the admission should be correct", function () {
+		pm.expect(bookedAdmission.price.amount).to.equal(offeredAdmission.price.amount);
+		pm.expect(bookedAdmission.price.currency).to.equal(offeredAdmission.price.currency);
+		pm.expect(bookedAdmission.price.scale).to.equal(offeredAdmission.price.scale);
+	});
+	
+	//products should be the same
+	pm.test("Products of the admission should be correct", function () {
+		bookedAdmission.products.forEach(function(bookedProduct){
+			var found = offeredAdmission.products.some(function(offeredProduct){
+
+				if(bookedProduct.productId==offeredProduct.productId){
+					return true;
+				}
+			});
+			
+			pm.expect(found).to.equal(true);
+		});
+	});
+	
+	//exchangeable
+	if(bookedAdmission.exchangeable!=undefined){
+		pm.test("Exchangable should be set and similar to offered", function () {
+			pm.expect(bookedAdmission.exchangeable).to.equal(offeredAdmission.exchangeable);
+		});
+	}
+	
+	//isReservationRequired
+	if(bookedAdmission.isReservationRequired!=undefined){
+		pm.test("isReservationRequired should be set and similar to offered", function () {
+			pm.expect(bookedAdmission.isReservationRequired).to.equal(offeredAdmission.isReservationRequired);
+		});
+	}
+	
+	//isReusable
+	if(bookedAdmission.isReusable!=undefined){
+		pm.test("Exchangable should be set and similar to offered", function () {
+			pm.expect(bookedAdmission.isReusable).to.equal(offeredAdmission.isReusable);
+		});
+	}
+	
+	//offerMode
+	if(bookedAdmission.offerMode!=undefined){
+		pm.test("offerMode should be set and similar to offered", function () {
+			pm.expect(bookedAdmission.offerMode).to.equal(offeredAdmission.offerMode);
+		});
+	}
+	
+	//refundable
+	if(bookedAdmission.refundable!=undefined){
+		pm.test("refundable should be set and similar to offered", function () {
+			pm.expect(bookedAdmission.refundable).to.equal(offeredAdmission.refundable);
+		});
+	}
+	
+	//passengerIds should match the passenger id in the passenger object of the booking, 
+	//with the reference still matching the offer
+	pm.test("Correct passengers are part of the admission", function () {
+		bookedAdmission.passengerIds.forEach(function(passengerId){
+			var found = booking.passengers.some(function(bookedPassenger){
+				if(passengerId==bookedPassenger.id){
+					return true;
+				}
+			});
+			
+			pm.expect(found).to.equal(true);
+		});
+		
+		
+	});
+	
+	return true;
+};
+
+compareReservations = function(bookedReservation, offeredReservation, booking){
+	validationLogger("Comparing reservation "+bookedReservation.id+" to "+offeredReservation.id);
+	//price "should" be the same
+	pm.test("Price of the reservation should be correct", function () {
+		pm.expect(bookedReservation.price.amount).to.equal(offeredReservation.price.amount);
+		pm.expect(bookedReservation.price.currency).to.equal(offeredReservation.price.currency);
+		pm.expect(bookedReservation.price.scale).to.equal(offeredReservation.price.scale);
+	});
+	
+	//products should be the same when available
+	if(bookedReservation.products!=undefined&&bookedReservation.products!=null&&bookedReservation.products.length!=0){
+		pm.test("Products of the reservation should be correct", function () {
+			bookedReservation.products.forEach(function(bookedProduct){
+				var found = offeredReservation.products.some(function(offeredProduct){
+	
+					if(bookedProduct.productId==offeredProduct.productId){
+						return true;
+					}
+				});
+				
+				pm.expect(found).to.equal(true);
+			});
+		});
+	}
+	
+	//exchangeable
+	if(bookedReservation.exchangeable!=undefined){
+		pm.test("Exchangable should be set and similar to offered", function () {
+			pm.expect(bookedReservation.exchangeable).to.equal(offeredReservation.exchangeable);
+		});
+	}
+	
+	//isReservationRequired
+	if(bookedReservation.isReservationRequired!=undefined){
+		pm.test("isReservationRequired should be set and similar to offered", function () {
+			pm.expect(bookedReservation.isReservationRequired).to.equal(offeredReservation.isReservationRequired);
+		});
+	}
+	
+	//isReusable
+	if(bookedReservation.isReusable!=undefined){
+		pm.test("Exchangable should be set and similar to offered", function () {
+			pm.expect(bookedReservation.isReusable).to.equal(offeredReservation.isReusable);
+		});
+	}
+	
+	//offerMode
+	if(bookedReservation.offerMode!=undefined){
+		pm.test("offerMode should be set and similar to offered", function () {
+			pm.expect(bookedReservation.offerMode).to.equal(offeredReservation.offerMode);
+		});
+	}
+	
+	//refundable
+	if(bookedReservation.refundable!=undefined){
+		pm.test("refundable should be set and similar to offered", function () {
+			pm.expect(bookedReservation.refundable).to.equal(offeredReservation.refundable);
+		});
+	}
+	
+	//passengerIds should match the passenger id in the passenger object of the booking, 
+	//with the reference still matching the offer
+	pm.test("Correct passengers are part of the reservation", function () {
+		bookedReservation.passengerIds.forEach(function(passengerId){
+			var found = booking.passengers.some(function(bookedPassenger){
+				if(passengerId==bookedPassenger.id){
+					return true;
+				}
+			});
+			
+			pm.expect(found).to.equal(true);
+		});
+		
+		
+	});
+	
+	return true;
+};
+
+compareAncillaries = function(ancillary1, ancillary2, booking){
+	validationLogger("Comparing ancillary "+ancillary1.id+" to "+ancillary2.id);
+	return true;
+};
+
+validationLogger = function (message) {
+	if(pm.globals.get("LOGGING_TYPE")=="FULL") {
+		console.log(message);
+	}
+};
+
+checkGenericBookedOfferPart = function(offerPart){
+	//check the creation date
+	var currentDate = new Date();
+	var createdOn = new Date(offerPart.createdOn);
+	var confirmableUntil = new Date(offerPart.confirmableUntil);
+
+	
+	pm.test("Correct createdOn is returned on bookedofferpart", function () {
+        pm.expect(currentDate.getDate()).to.equal(createdOn.getDate());
+        pm.expect(currentDate.getMonth()).to.equal(createdOn.getMonth());
+        pm.expect(currentDate.getFullYear()).to.equal(createdOn.getFullYear());
+    });
+    
+    //check the confirmableUntil
+    pm.test("a correct confirmableUntil is returned on bookedofferpart", function () {
+    	var current = currentDate.getTime();
+    	var confirmation = confirmableUntil.getTime();
+        pm.expect(confirmation).to.be.above(current);
+    });
+    
+    //check the status
+    pm.test("a correct status is returned on bookedofferpart", function () {
+        pm.expect(offerPart.status).to.equal("PREBOOKED");
+    });
+    
+};
 
